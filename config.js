e9067b7739ffd13463f2a2a3a17c93964c60a5a0
BoredLama <43473329+BoredLama@users.noreply.github.com>
2019-02-15 15:22:20 +0200

    Config logic

---
+++ config.js
@@ -0,0 +1,71 @@
+const commentJson = require('comment-json')
+
+const path = require('path')
+const rootDir = path.dirname(process.execPath)
+
+const fs = require('fs')
+
+const configFile = 'config.json'
+
+const defaultConfig = {
+
+	"// autoLaunch": [["// if this is set to true, the add-on will run on system start-up"]],
+	"autoLaunch": false,
+
+	"// responseTimeout": [["// for stremio add-on, in milliseconds, if timeout is reached it will respond with whatever results it already has, 0 = no timeout"]],
+	"responseTimeout": 11000,
+
+	"// addonPort": [["// port to use for stremio add-on, default is 7010"]],
+	"addonPort": 7010,
+
+	"// googleResults": [["// maximum number of results to parse from Google, default is 25"]],
+	"googleResults": 25,
+
+	"// remote": [["// make add-on available remotely too, through LAN and the Internet"]],
+	"remote": false,
+
+	"// subdomain": [["// set the preferred subdomain (if available), only applicable if remote is set to true"]],
+	"subdomain": false,
+
+	"page": {
+
+		"// readTimeout": [["// read timeout in milliseconds for http requests to jackett server, 0 = no timeout"]],
+		"readTimeout": 10000,
+
+		"// openTimeout": [["// open timeout in milliseconds for http requests to jackett server, 0 = no timeout"]],
+		"openTimeout": 10000
+
+	}
+}
+
+const readConfig = () => {
+
+	const configFilePath = path.join(rootDir, configFile)
+
+	if (fs.existsSync(configFilePath)) {
+		var config
+
+		try {
+			config = fs.readFileSync(configFilePath)
+		} catch(e) {
+			// ignore read file issues
+			return defaultConfig
+		}
+
+		return commentJson.parse(config.toString())
+	} else {
+		const configString = commentJson.stringify(defaultConfig, null, 4)
+
+		try {
+			fs.writeFileSync(configFilePath, configString)
+		} catch(e) {
+			// ignore write file issues
+			return defaultConfig
+		}
+
+		return readConfig()
+	}
+
+}
+
+module.exports = readConfig()
